c0b1c4bba1ad504511cb1ba70ec49b4842146f31
Joff Thyer <jsthyer@thyer.org>
2015-01-15 15:44:24 -0500

    radix tree incorporated into logstats

--- logstats.py
+++ logstats.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import radix
 import sys
 import re
 import os
-import math
 import struct
 import socket
 import sqlite3
@@ -18,7 +18,8 @@ class RIRLogStats:
         self.dbh = sqlite3.connect(self.dbname)
         self.dbh.text_factory = str
         self.records = []
-        self.ip2cc = {}
+        self.rib = radix.Radix()
+        self.country = {}
         self.freq = {}
 
     def _cidr2mask(self, cidr):
@@ -40,40 +41,19 @@ class RIRLogStats:
                 return True
         return False
 
-    def _ip2cc(self, ip):
-        if ip in self.ip2cc.keys():
-            return self.ip2cc[ip]
-        b_ip = struct.unpack('!L', socket.inet_pton(socket.AF_INET, ip))[0]
-        for r in self.records:
-            # calculate binary IP range
-            cc = r[0]
-            country_name = r[1]
-            rirtype = r[5]
-            if rirtype == 'ipv4':
-                _, temp = r[2].split('/')
-                cidr = int(temp)
-                _32bits = long(math.pow(2, 32) - 1)
-                b_mask = _32bits << (32 - cidr) & _32bits
-                b_network = struct.unpack(
-                    '!L',
-                    socket.inet_pton(socket.AF_INET, r[3])
-                )[0]
-                b_broadcast = (b_network | ~b_mask) & _32bits
-                if b_ip >= b_network and b_ip <= b_broadcast:
-                    self.ip2cc[ip] = {'cc': cc, 'country_name': country_name}
-                    return self.ip2cc[ip]
-            else:
-                continue
-
     def _iptables_log(self, options):
         f = open(options.iptables, 'r')
         data = f.read()
         f.close()
 
-        if options.src:
-            rxp = r'.+SRC=((\d{1,3}\.){3}\d{1,3})'
-        elif options.dst:
-            rxp = r'.+DST=((\d{1,3}\.){3}\d{1,3})'
+        if options.ipv4:
+            if options.src:
+                rxp = r'.+SRC=((\d{1,3}\.){3}\d{1,3})'
+            elif options.dst:
+                rxp = r'.+DST=((\d{1,3}\.){3}\d{1,3})'
+        elif options.ipv6:
+            print 'IPv6 is not yet supported'
+            return
 
         total = 0
         for line in data.split('\n'):
@@ -82,24 +62,26 @@ class RIRLogStats:
                 continue
             elif self._RFC1918(m.group(1)):
                 continue
-            if m.group(1) not in self.freq:
-                self.freq[m.group(1)] = 1
-            else:
-                self.freq[m.group(1)] += 1
+            rib_entry = self.rib.search_best(m.group(1))
+            if rib_entry:
+                cc = rib_entry.data['cc']
+                if cc not in self.freq:
+                    self.freq[cc] = 1
+                else:
+                    self.freq[cc] += 1
             total += 1
 
         print """
- Top %d IPTABLES Firewall Drops by Source Address
+ Top %d IPTABLES Firewall Hits by Source Country
 ------------------------------------------------------------------\
 """ % (int(options.top))
-        top = 0
+        top = 1
         for r in sorted(self.freq, key=self.freq.__getitem__, reverse=True):
             if top > int(options.top):
                 break
-            ccd = self._ip2cc(r)
             percent = (float(self.freq[r]) / total) * 100.0
-            print '%02d: %20s | %-15s | hits = %5d (%5.2f%%)' % \
-                (top, ccd['country_name'], r, self.freq[r], percent)
+            print '%02d: %30s | hits = %5d (%5.2f%%)' % \
+                (top, self.country[r], self.freq[r], percent)
             top += 1
         print """\
 ------------------------------------------------------------------"""
@@ -124,8 +106,20 @@ AND (rir.status = 'assigned' or rir.status = 'allocated')
 ORDER BY rir.cc, rir.type, rir.start_binary ASC
 """ % (sql_type)
         cur.execute(sql)
-        for row in cur.fetchall():
-            self.records.append(row)
+        for r in cur.fetchall():
+            cc = r[0]
+            country_name = r[1]
+            rirtype = r[5]
+            if rirtype == 'ipv4':
+                rnode = self.rib.add(r[2])
+            elif rirtype == 'ipv6':
+                rnode = self.rib.add('%s/%s' % (r[3], r[4]))
+            else:
+                continue
+            if cc not in self.country:
+                self.country[cc] = country_name
+            rnode.data['cc'] = cc
+            rnode.data['country_name'] = country_name
 
     def run(self, options):
         self._get_dbrecords(options)
