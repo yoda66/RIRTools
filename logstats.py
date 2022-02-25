a59df7a560d0f8bde91c1d9227d8229902b642ac
Joff Thyer <jsthyer@gmail.com>
2022-02-25 15:51:57 -0500

    updated logstats

--- logstats.py
+++ logstats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import radix
@@ -13,8 +13,8 @@ import sqlite3
 class RIRLogStats:
 
     def __init__(self):
-        dbhome = '%s/.rirdb' % (os.path.expanduser('~'))
-        self.dbname = '%s/rir.db' % (dbhome)
+        dbhome = '{}/.rirdb'.format(os.path.expanduser('~'))
+        self.dbname = '{}/rir.db'.format(dbhome)
         self.dbh = sqlite3.connect(self.dbname)
         self.dbh.text_factory = str
         self.records = []
@@ -46,10 +46,11 @@ class RIRLogStats:
 
     def _print_freq_summary(self, title, total):
         if str(options.top).lower() == "all":
-            header = "\nAll %s Firewall Hits by Source Country\n" % ( title )
+            header = '\nAll {} Firewall Hits by Source Country\n'.format(title)
         else:
-            header = "\n Top %d %s Firewall Hits by Source Country\n" % ( int(options.top), title)
-        print header
+            header = '\n Top {:d} {} Firewall Hits by Source Country\n'.format(
+                int(options.top), title)
+        print(header)
 
         top = 1
         for r in sorted(self.freq, key=self.freq.__getitem__, reverse=True):
@@ -57,20 +58,21 @@ class RIRLogStats:
                 if top > int(options.top):
                     break
             percent = (float(self.freq[r]) / total) * 100.0
-            print '%02d: %30s | hits = %8d (%5.2f%%)' % \
-                (top, self.country[r], self.freq[r], percent)
+            print('{:02d}: {:30s} | hits = {:8d} ({:5.2f}%)'.format(
+                top, self.country[r], self.freq[r], percent))
             top += 1
-        print """\
-------------------------------------------------------------------"""
+        print("""\
+------------------------------------------------------------------""")
 
     def _asa_log(self, options):
-        r_ip = '((\d{1,3}\.){3}\d{1,3})'
-        r_generic = '[ a-zA-Z:\(\)_\-]+'
-        r_proto = '[A-Za-z]{2,4}'
-        r_deny = r'^.+Deny\s%s\s%s%s/\d{1,5}%s%s.+$' % \
-            (r_proto, r_generic, r_ip, r_generic, r_ip)
-        r_built = r'^.+Built%s\d+%s%s/\d{1,5}\s\(%s/\d{1,5}\)%s%s/\d{1,5}.+$' % \
-            (r_generic, r_generic, r_ip, r_ip, r_generic, r_ip)
+        r_ip = r'((\d{1,3}\.){3}\d{1,3})'
+        r_generic = r'[ a-zA-Z:\(\)_\-]+'
+        r_proto = r'[A-Za-z]{2,4}'
+        r_deny = r'^.+Deny\s{}\s{}{}/\d{{1,5}}{}{}.+$'.format(
+            r_proto, r_generic, r_ip, r_generic, r_ip)
+        r_built = r'''\
+^.+Built{}\d+{}{}/\d{{1,5}}\s\({}/\d{{1,5}}\){}{}/\d{{1,5}}.+$'''.format(
+            r_generic, r_generic, r_ip, r_ip, r_generic, r_ip)
         if options.ipv4:
             if options.asa_allow:
                 rxp = re.compile(r_built)
@@ -179,10 +181,10 @@ SELECT  rir.cc, country_codes.name,
 FROM rir
 LEFT JOIN country_codes
 ON country_codes.cc = rir.cc
-%s
+{}
 AND (rir.status = 'assigned' or rir.status = 'allocated')
 ORDER BY rir.cc, rir.type, rir.start_binary ASC
-""" % (sql_type)
+""".format(sql_type)
         cur.execute(sql)
         for r in cur.fetchall():
             cc = r[0]
@@ -191,7 +193,7 @@ ORDER BY rir.cc, rir.type, rir.start_binary ASC
             if rirtype == 'ipv4':
                 rnode = self.rib.add(r[2])
             elif rirtype == 'ipv6':
-                rnode = self.rib.add('%s/%s' % (r[3], r[4]))
+                rnode = self.rib.add('{}/{}'.format(r[3], r[4]))
             else:
                 continue
             if cc not in self.country:
@@ -201,19 +203,18 @@ ORDER BY rir.cc, rir.type, rir.start_binary ASC
 
     def _verify_file(self, options):
         if options.iptables:
-            file=options.iptables
+            file = options.iptables
         elif options.asa:
-            file=options.asa
+            file = options.asa
         elif options.ipf:
-            file=options.ipf
+            file = options.ipf
         else:
-            print "No File specified"
+            print("No File specified")
             sys.exit(1)
 
         if not os.path.isfile(file) and str(file) != "-":
-            print "File NOT Found: %s" % ( file )
+            print("File NOT Found: {}".format(file))
             sys.exit(1)
-
 
     def run(self, options):
         self._verify_file(options)
@@ -231,11 +232,11 @@ if __name__ == '__main__':
     VERSION = '20150122_1105'
     desc = """
 ----------------------------------
- %s version %s
+ {} version {}
  Author: Joff Thyer (c) 2015
  Black Hills Information Security
 ----------------------------------
-""" % (os.path.basename(sys.argv[0]), VERSION)
+""".format(os.path.basename(sys.argv[0]), VERSION)
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=desc
@@ -276,21 +277,20 @@ if __name__ == '__main__':
 
     if not (options.ipv4 or options.iptables):
         parser.print_help()
-        print '\nERROR: Please specify the --ipv4 flag and a log format'
+        print('\nERROR: Please specify the --ipv4 flag and a log format')
         sys.exit(1)
 
     if not (options.src or options.dst):
         options.src = True
 
-    print '%s' % (desc)
+    print('{}'.format(desc))
     if not options.iptables and not options.asa and not options.ipf:
-        print "Syntax error:"
-        print "\tYou must specify at least one of:"
-        print "\t\t--iptables [LOGFILE|-]"
-        print "\t\t--asa [LOGFILE|-]\n"
-        print "\t\t--ipf [LOGFILE|-]"
-        print "\tTry running %s -h\n" % ( sys.argv[0] )
+        print("Syntax error:")
+        print("\tYou must specify at least one of:")
+        print("\t\t--iptables [LOGFILE|-]")
+        print("\t\t--asa [LOGFILE|-]\n")
+        print("\t\t--ipf [LOGFILE|-]")
+        print("\tTry running {} -h\n".format(sys.argv[0]))
         exit()
-
     logstats = RIRLogStats()
     logstats.run(options)
